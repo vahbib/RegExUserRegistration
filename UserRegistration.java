75afdaaad7afd483b640b56cd029c36c5a1c41a1
vahbib <singhbibhav084@gmail.com>
2020-10-04 22:48:27 +0530

    Added Day9 Problem - All UserCases

---
+++ UserRegistration.java
@@ -0,0 +1,68 @@
+package com.day9.regex.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+	static Scanner sc = new Scanner(System.in);
+
+	// UC1 - First name starts with Cap and has minimum 3 characters
+	public static void firstName() {
+		System.out.println("Enter First Name");
+		String fN = sc.nextLine();
+		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", fN) == true)
+			System.out.println("Valid First Name");
+		else
+			System.out.println("Invalid First Name");
+	}
+
+	// UC2 - Last name starts with Cap and has minimum 3 characters
+	public static void lastName() {
+		System.out.println("Enter Last Name");
+		String lN = sc.nextLine();
+		if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
+			System.out.println("Valid Last Name");
+		else
+			System.out.println("Invalid Last Name");
+	}
+
+	/* UC 3 - enter a valid email
+	 * UC 9 - Cleared All Sample Provided 
+	 */
+	public static void email() {
+		System.out.println("Enter EMail");
+		String email = sc.nextLine();
+		if (Pattern.matches("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*$", email) == true)
+			System.out.println("Valid Email");
+		else
+			System.out.println("Invalid Email");
+	}
+
+	// UC4 - Mobile Format
+	public static void mobile() {
+		System.out.println("Enter Mobile Number With Country Code(space between code and number)");
+		String m = sc.nextLine();
+		if (Pattern.matches("^[1-9]{2}\s{1}[1-9]{1}[0-9]{9}$", m) == true)
+			System.out.println("Valid Mobile");
+		else
+			System.out.println("Invalid Mobile");
+	}
+
+	// UC 5 to 8 - Password
+	public static void password() {
+		System.out.println("Enter the Password");
+		String p = sc.nextLine();
+		if (Pattern.matches("^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,}$", p) == true)
+			System.out.println("Valid Password");
+		else
+			System.out.println("Invalid Password");
+	}
+
+	public static void main(String[] args) {
+		firstName();
+		lastName();
+		email();
+		mobile();
+		password();
+	}
+}
